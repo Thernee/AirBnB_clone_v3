6338f43a7d1870d5dc88f82aa2eb0bad10db4840
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-28 17:19:26 +0000

    Added api and v1 directories

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Set up API with flask."""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """Close active session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    port = getenv('HBNB_API_PORT', default=5000)
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    app.run(host, int(port))
