f0fcba3235184c6c0ca80692458fd0ba6aed66d2
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-29 13:00:51 +0000

    updated api/v1/views/index.py for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def check_status():
     """Return JSON status."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count_by_type():
+    """Retrieve number of each object by type"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
