92cb7e40c467b4b4868d46de25f104fc0f0a95da
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-29 12:36:07 +0000

    Fixed: api/v1/views/index.py api/v1/app.py and api/v1/views/__init__.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(err):
     """Close active session."""
     storage.close()
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,8 @@
 
 """Index file for 'views' dir."""
 
-from api.views import app_views
+from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
